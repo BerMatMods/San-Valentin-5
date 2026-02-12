82204e9f65d6901e5fff0cf435855dad1ce4cfd3
AnthZz Berrocal <berrocalanthony1@gmail.com>
2026-02-11 20:02:02 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,77 @@
+const febHolidays = [
+"Mi bebe preciosa 👶 ",
+"❤️ Tengo muchas cosas q quiero decirle",
+"❤️ Primero que nada que la amooo ❤️",
+"💑 La quiero y me encanta",
+" Me gusta mucho aun que ya lo sabe 😍",
+"💍 Cada segundo que transcurre mas la quiero 🤭",
+"Y cada que no esta siento que muero 😵",
+"Me encantan sus ojos que tanto brillan 🤩",
+"Sus labios, lunares y sonrisa 👄🤤",
+"100pre se ve tan bella 😏",
+"😍 Quiero ser su caballero y usted mi princesa 👑",
+"💏 Me encanta estar con usted",
+"❤️ Darle un abraso 🤗",
+"Decirle que me encanta y llenarle de besos 😘",
+"🌟 Quiero tenerla a mi lado por 100pre 👩‍❤️‍👨",
+"Quiero ser suyo hasta la muerte 🥰",
+"😍 Mi princesa hermosa 👑",
+"❤️ Es otro nivel ❤️",
+"🏆 ¡¡Es la  mejor mi bebé ❤️!!",
+"❤️ El amor de mi vida ❤️",
+"⌛ Me encata mas cada segundo ❤️",
+"La extraño mucho 😔",
+" Ya quiero verla 🥹",
+"❤️ Que me llene de besos 👉👈",
+"❤️ Y de abrazos 🥺",
+"🥂 Cuando nos veamos hay q festejar ❤️",
+"💞 Si mi amor",
+"Espero y sea pronto ❤️",
+"😘 Le mando muchos bechitos ❤️",
+"💖 Es la mejor mi vida no lo olvide 👌❤️",
+"❤️ Feliz San Valentin ❤️"
+];
+const ulEl = document.querySelector("ul");
+const d = new Date();
+let daynumber = d.getMonth() == 1 ? d.getDate() - 1 : 0;
+let activeIndex = daynumber;
+const rotate = -360 / febHolidays.length;
+init();
+function init() {
+  febHolidays.forEach((holiday, idx) => {
+    const liEl = document.createElement("li");
+    liEl.style.setProperty("--day_idx", idx);
+    liEl.innerHTML = `<time datetime="2022-02-${idx + 1}">${
+      idx + 1
+    }</time><span>${holiday}</span>`;
+    ulEl.append(liEl);
+  });
+  ulEl.style.setProperty("--rotateDegrees", rotate);
+  adjustDay(0);
+}
+function adjustDay(nr) {
+  daynumber += nr;
+  ulEl.style.setProperty("--currentDay", daynumber);
+  const activeEl = document.querySelector("li.active");
+  if (activeEl) activeEl.classList.remove("active");
+  activeIndex = (activeIndex + nr + febHolidays.length) % febHolidays.length;
+  const newActiveEl = document.querySelector(
+    `li:nth-child(${activeIndex + 1})`
+  );
+  document.body.style.backgroundColor = window.getComputedStyle(
+    newActiveEl
+  ).backgroundColor;
+  newActiveEl.classList.add("active");
+}
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      adjustDay(-1);
+      break;
+    case "ArrowDown":
+      adjustDay(1);
+      break;
+    default:
+      return;
+  }
+});
